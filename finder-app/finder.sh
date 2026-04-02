04d44b0b1d2eeeaefbf211715d56172089c97988
Cornelia Schulz <cornelia.schulz@web.de>
2026-04-02 10:56:04 +0200

    implement finder.sh

---
+++ finder-app/finder.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+filesdir=$1
+searchstr=$2
+usage="$0 <filesdir> <searchstr>"
+
+if [ ! $# -eq 2 ]
+then
+    echo "Wrong number of arguments given. Usage: ${usage}."
+    exit 1
+fi
+
+if [ -z ${filesdir} ] || [ -z ${searchstr} ]
+then
+    echo "At least one parameter was not specified. Usage: ${usage}."
+    exit 1
+fi
+
+if [ ! -d ${filesdir} ]
+then
+    echo "Parameter ${filesdir} does not specify an existing directory. Usage: ${usage}."
+    exit 1
+fi
+
+files=$(find ${filesdir} -type f | wc -l)
+matching_lines=$(egrep ${searchstr} ${filesdir} -r | wc -l)
+
+echo "The number of files are ${files} and the number of matching lines are ${matching_lines}."
+exit 0
